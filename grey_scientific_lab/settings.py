575d5f234b82825be7f305ac2231f75fa107a041
kishoretocs <kishoretocs@gmail.com>
2024-08-18 22:31:53 +0530

    third

--- grey_scientific_lab/settings.py
+++ grey_scientific_lab/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1eu56qr&b5qwv+v*&ixde*zi=x63x2%eu**pk&=hv@^!5mrinh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
